0822782f81f28e99aea4f1be3b56a6d41e245d90
Mikael Karlsson <mikael.karlsson@nordcloud.com>
2023-01-19 16:55:01 +0100

    new run7

--- main.tf
+++ main.tf
@@ -107,14 +107,14 @@ resource "azurerm_app_service" "app" {
   app_service_plan_id = azurerm_app_service_plan.asp.id
   app_settings = {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-    "DOCKER_REGISTRY_SERVER_URL"          = "containerregistrymkk2000.azurecr.io"
+    "DOCKER_REGISTRY_SERVER_URL"          = "${azurerm_container_registry.container_registry.name}.azurecr.io"
     "DOCKER_REGISTRY_SERVER_USERNAME"     = azurerm_container_registry.container_registry.admin_username
     "DOCKER_REGISTRY_SERVER_PASSWORD"     = azurerm_container_registry.container_registry.admin_password
 
 
   }
   site_config {
-    linux_fx_version = "DOCKER|${azurerm_container_registry.container_registry.name}.azurecr.io/mkk:latest"
+    linux_fx_version = "DOCKER|containerregistrymkk2000.azurecr.io/mkk:latest"
 
   }
   identity {
