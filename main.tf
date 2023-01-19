a375df5774eccb0665c537f9a244d259d7a049e8
Mikael Karlsson <mikael.karlsson@nordcloud.com>
2023-01-19 10:43:11 +0100

    added storage & acr

--- main.tf
+++ main.tf
@@ -22,3 +22,38 @@ resource "azurerm_resource_group" "rg" {
     location = var.location
   
 }
+
+resource "azurerm_storage_account" "storage" {
+    name = "azstorage"
+    resource_group_name = var.RGName
+    location = var.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+    allow_blob_public_access = true
+    depends_on = [
+      azurerm_resource_group.rg
+    ]
+
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "container1"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "blob"
+  depends_on = [
+    azurerm_storage_account.storage
+  ]
+
+}
+
+resource "azurerm_container_registry" "container_registry" {
+    name = "containerregistry"
+    resource_group_name = var.RGName
+    location = var.location
+    sku = "Basic"
+    admin_enabled = true
+    depends_on = [
+      azurerm_resource_group.rg
+    ] 
+}
+
