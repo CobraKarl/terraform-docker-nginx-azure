0ed99829a1bda30cc9b20a2bc6a38abf34b53d60
Mikael Karlsson <mikael.karlsson@nordcloud.com>
2023-01-23 11:05:54 +0100

    final2.0

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ resource "azurerm_app_service" "app" {
 
   }
   site_config {
-    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.admin_username}/mkk:latest"
+    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.name}/mkk:latest"
 
   }
   identity {
